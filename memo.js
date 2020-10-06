21d9f9c7ab067a8923f3b9a8f62cfafd39c5ceda
yaju1919 <46996104+yaju1919@users.noreply.github.com>
2020-10-06 14:38:29 +0900

    Create memo.js

---
+++ memo.js
@@ -0,0 +1,165 @@
+(function() {
+    'use strict';
+    var cv_width = 550;
+    var prev = {
+        x: 0,
+        y: 0
+    };
+    function draw(e){
+        e.preventDefault();
+        var x = e.offsetX,
+            y = e.offsetY;
+        var size = inputPenSize();
+        var touchFlag = e.type.indexOf("touch") !== -1;
+        if(touchFlag){
+            x = e.originalEvent.touches[0].pageX - cv.offset().left;
+            y = e.originalEvent.touches[0].pageY - cv.offset().top;
+            if(e.type === "touchstart"){
+                prev.x = x;
+                prev.y = y;
+            }
+        }
+        if(e.which || touchFlag){
+            var ctx = this.getContext('2d');
+            var ctx_master = cv_master.get(0).getContext('2d');
+            if(e.buttons === 2 || eraseFlag()){
+                ctx = ctx_master;
+                ctx_master.globalCompositeOperation = 'destination-out';
+            }
+            else ctx_master.globalCompositeOperation = 'source-over';
+            ctx.beginPath();
+            ctx.moveTo(prev.x, prev.y);
+            ctx.lineTo(x,y);
+            ctx.lineWidth = size;
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
+            ctx.closePath();
+            ctx.fillStyle = "black";
+            ctx.fill();
+        }
+        prev.x = x;
+        prev.y = y;
+    }
+    function setCSSfilter(){
+        var rgb = yaju1919.getRGB(jqInputColor.val());
+        cv.css({
+            filter: new Solver(new Color(rgb[0], rgb[1], rgb[2])).solve().filter + ` opacity(${inputColorAlpha()*100}%)`
+        });
+    }
+    function commitCanvas(){
+        var ctx = cv.get(0).getContext('2d');
+        var imageData = ctx.getImageData(0, 0, cv.width(), cv.height());
+        var data = imageData.data;
+        var rgb = yaju1919.getRGB(jqInputColor.val());
+        var a = inputColorAlpha();
+        for (var i = 0; i < data.length; i += 4) {
+            if(data[i + 3] === 0) continue;
+            data[i] = rgb[0];
+            data[i + 1] = rgb[1];
+            data[i + 2] = rgb[2];
+            data[i + 3] *= a;
+        }
+        ctx.putImageData(imageData, 0, 0);
+        cv_master.get(0).getContext('2d').drawImage(cv.get(0),0,0);
+        ctx.clearRect(0, 0, cv.width(), cv.height());
+    }
+    var stack = [];
+    function fill(x,y){
+        var ctx = cv.get(0).getContext('2d');
+        var w = cv.width();
+        var imageData = ctx.getImageData(0, 0, cv.width(), cv.height());
+        var data = imageData.data;
+        stack.push([x,y,judgeType(data,w,x,y)]);
+        while(true){
+            var xy = stack.pop();
+        }
+    }
+    function judgeType(data,w,x,y){
+        data[transBin(w,x,y)];
+    }
+    function transBin(w,x,y){
+        return (x+y*w)*4;
+    }
+    function addBtn(h,title,func){
+        $("<button>").appendTo(h).text(title).click(func);
+    }
+    var h = $("<div>").attr({
+        onselectstart: "return false"
+    }).css({
+        "text-align": "center",
+        padding: "1em",
+    }).appendTo($("body").css({
+        backgroundColor: "lightblue"
+    }));
+    $("<h1>").appendTo(h).text("お絵かきツール");
+    $("<div>").appendTo(h).text("右クリックで消しゴム");
+    var h_cv = $("<div>").appendTo(h).css({
+        display: "inline-block",
+        position: "relative",
+        backgroundColor: "white",
+        width: cv_width,
+        height: cv_width,
+    });
+    var cv_master = $("<canvas>").appendTo(h_cv).css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+    }).attr({
+        width: cv_width,
+        height: cv_width
+    });
+    var cv = $("<canvas>").appendTo(h_cv).css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+    }).attr({
+        width: cv_width,
+        height: cv_width
+    }).bind("contextmenu",function(e){
+        return false;
+    }).on("mousedown mousemove touchstart touchmove",draw);
+    $(window).on("mouseup touchend",commitCanvas);
+    //--------------------------------------------------------------------------------------------------
+    var h_ui = $("<div>").appendTo(h).css({
+        backgroundColor: "white",
+    });
+    var inputPenSize = yaju1919.addInputRange(h_ui,{
+        title: "ペンの太さ",
+        min: 1,
+        max: 30,
+        step: 1,
+        value: 5,
+        save: "ペンの太さ"
+    });
+    var jqInputColor = $("<input>").appendTo($("<div>").appendTo(h_ui).text("色：")).attr({
+        type: "color"
+    }).on("change",setCSSfilter);
+    var inputColorAlpha = yaju1919.addInputRange(h_ui,{
+        title: "不透明度",
+        min: 0,
+        max: 1,
+        step: 0.01,
+        value: 1,
+        save: "不透明度",
+        change: function(){
+            if(inputColorAlpha) setCSSfilter();
+        }
+    });
+    setCSSfilter();
+    var eraseFlag = yaju1919.addInputBool(h_ui,{
+        title: "消しゴム",
+        save: "消しゴム",
+    });
+    addBtn(h_ui,"全消し",function(){
+        cv_master.get(0).getContext('2d').clearRect(0, 0, cv.width(), cv.height());
+    });
+    addBtn(h_ui,"保存",function(){
+        $("<a>").attr({
+            href: cv_master.get(0).toDataURL(),
+            download: "イラスト.png"
+        }).get(0).click();
+    });
+    h_ui.children().each((i,e)=>$(e).add("<br>"));
+})();
